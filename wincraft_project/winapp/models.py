5f7f34c8029c9902a38d3bef00b13b8fb976c0ca
rajabhishek9271 <rajabhishek9271@gmail.com>
2020-07-29 23:11:50 +0530

    query table created

--- wincraft_project/winapp/models.py
+++ wincraft_project/winapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Queries(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True,null=True)
+    phone = models.PositiveIntegerField(blank=True,null=True)
+    query = models.CharField(max_length=1000)
+    
