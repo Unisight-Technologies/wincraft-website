0cb1dce1493dee44edd954b46600bb656a25febe
rajabhishek9271 <rajabhishek9271@gmail.com>
2020-07-30 23:02:32 +0530

    Phone field changed

--- wincraft_project/winapp/models.py
+++ wincraft_project/winapp/models.py
@@ -4,6 +4,5 @@ from django.db import models
 class Queries(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(blank=True,null=True)
-    phone = models.PositiveIntegerField(blank=True,null=True)
+    phone = models.BigIntegerField(blank=True,null=True)
     query = models.CharField(max_length=1000)
-    
