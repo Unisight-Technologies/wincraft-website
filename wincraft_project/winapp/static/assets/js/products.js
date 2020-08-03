32e67a5c327fc2ff477b75837a97ce720e5d4bbc
Naitik0909 <parmarnaitik0909@gmail.com>
2020-08-03 22:33:28 +0530

    Sliding animation added to products.js

--- wincraft_project/winapp/static/assets/js/products.js
+++ wincraft_project/winapp/static/assets/js/products.js
@@ -1,46 +1,27 @@
-var counter_aluk = 0;
-var counter_firat = 0;
-var counter_simta = 0;
-
-
 function showContent(brand){
 
   if (brand == 'aluk'){
-      counter_aluk++;
-      if(counter_aluk % 2 == 0){
-        $('#aluk').css('display', 'none');
-      }
-      else{
-        $('#simta').css('display', 'none');
-        $('#firat').css('display', 'none');
-        $('#aluk').css('display', 'inherit');
-      }
+
+      $('#simta').css('display', 'none');
+      $('#firat').css('display', 'none');
+      $('#aluk').slideToggle();
+
   }
 
 
   if (brand == 'simta'){
-      counter_simta++;
-      if(counter_simta % 2 == 0){
-        $('#simta').css('display', 'none');
-      }
-      else{
-        $('#aluk').css('display', 'none');
-        $('#firat').css('display', 'none');
-        $('#simta').css('display', 'inherit');
-      }
+
+      $('#aluk').css('display', 'none');
+      $('#firat').css('display', 'none');
+      $('#simta').slideToggle();
   }
 
 
   if (brand == 'firat'){
-      counter_firat++;
-      if(counter_firat % 2 == 0){
-        $('#firat').css('display', 'none');
-      }
-      else{
-        $('#simta').css('display', 'none');
-        $('#aluk').css('display', 'none');
-        $('#firat').css('display', 'inherit');
-      }
+
+      $('#simta').css('display', 'none');
+      $('#aluk').css('display', 'none');
+      $('#firat').slideToggle();
   }
 
 }
