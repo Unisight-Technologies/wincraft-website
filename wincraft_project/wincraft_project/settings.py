3ed45c9f75e06b3646e8b1fd0cfea77f8870ede3
Naitik0909 <parmarnaitik0909@gmail.com>
2020-08-08 18:50:12 +0530

    Added aws to allowed host

--- wincraft_project/wincraft_project/settings.py
+++ wincraft_project/wincraft_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = '8gpu8k)x=-j^)s7j9@&)8cd@^e&$82d-uz%_=(t&zpjmucd$(n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if socket.gethostname() == 'ip-172-31-36-15':
+    DEBUG = False
+else:
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-19-213-170.us-east-2.compute.amazonaws.com', '127.0.0.1']
 
 
 # Application definition
