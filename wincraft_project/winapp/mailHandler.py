932fb9ef9451409179e4e65d43f93defbc11d34b
Naitik0909 <parmarnaitik0909@gmail.com>
2020-08-18 14:25:23 +0530

    Beautified views.py with emailHandler

---
+++ wincraft_project/winapp/mailHandler.py
@@ -0,0 +1,25 @@
+from django.core.mail import send_mail
+
+
+def sendMailToUser(name, send_to):
+    subject = "Regarding query sent on Wincraft Buildmat's Website"
+    message = "Hello "+name+"! \n\nWe have successfully received your query.\nWe are glad that you tried to connect with us.\n\nWe will get back to you as soon as possible.\n\nRegards\n-Team Wincraft Buildmat"
+    send_mail(
+        subject,
+        message,
+        'teamurbaninsight@gmail.com',
+        [send_to],
+        fail_silently = False,
+    )
+
+def sendMailToWincraft(name, email, phone, query):
+    message = "The following query has been received on our website:\n\nName: "+name+"\nEmail Id: "+email+"\nPhone Number: "+phone+"\nQuery: "+query+"\n\nRegards"
+    subject = "A query has been received on Wincraft Buildmat"
+    send_mail(
+        subject,
+        message,
+        'teamurbaninsight@gmail.com',
+        ['parmarnaitik0909@gmail.com'],
+        fail_silently = False,
+
+    )
--- wincraft_project/winapp/views.py
+++ wincraft_project/winapp/views.py
@@ -2,31 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from . import models
 from django.contrib import messages
-from django.core.mail import send_mail
-
-def sendMailToUser(name, send_to):
-    subject = "Regarding query sent on Wincraft Buildmat's Website"
-    message = "Hello "+name+"! \n\nWe have successfully received your query.\nWe are glad that you tried to connect with us.\n\nWe will get back to you as soon as possible.\n\nRegards\n-Team Wincraft Buildmat"
-    send_mail(
-        subject,
-        message,
-        'teamurbaninsight@gmail.com',
-        [send_to],
-        fail_silently = False,
-    )
-
-def sendMailToWincraft(name, email, phone, query):
-    message = "The following query has been received on our website:\n\nName: "+name+"\nEmail Id: "+email+"\nPhone Number: "+phone+"\nQuery: "+query+"\n\nRegards"
-    subject = "A query has been received on Wincraft Buildmat"
-    send_mail(
-        subject,
-        message,
-        'teamurbaninsight@gmail.com',
-        ['wincraftbuildmat@gmail.com'],
-        fail_silently = False,
-
-    )
-
+from . import mailHandler
 
 class Homepage(View):
     def get(self, request, *args, **kwargs):
@@ -47,8 +23,8 @@ class Homepage(View):
                     query=query
                 )
             new_query.save()
-            sendMailToUser(name, email)
-            sendMailToWincraft(name, email, phone, query)
+            mailHandler.sendMailToUser(name, email)
+            mailHandler.sendMailToWincraft(name, email, phone, query)
             messages.success(request, "Your query has been successfully submitted. We will get back to you soon.")
             return redirect("index")
 
